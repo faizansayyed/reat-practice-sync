c27e9666d9c3ded71e4f304964efc128c72c8f13
Faizan Sayyed <faizansayyed36@gmail.com>
2023-08-02 11:23:22 +0530

    updated design for custom cell renderer

--- src/CustomCellRenderer.jsx
+++ src/CustomCellRenderer.jsx
@@ -1,28 +1,78 @@
 // CustomCellRenderer.js
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import ClickAwayListener from '@mui/material/ClickAwayListener';
+import Popper from '@mui/material/Popper';
+import Paper from '@mui/material/Paper';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
 
 const CustomCellRenderer = ({ value, onDelete, onEdit }) => {
-  const [showMenu, setShowMenu] = useState(false);
+  const [open, setOpen] = useState(false);
+  const anchorRef = React.useRef(null);
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
 
   const handleDelete = () => {
-    onDelete(value); // Call the API to delete data
-    setShowMenu(false);
+    onDelete(value);
+    setOpen(false);
   };
 
   const handleEdit = () => {
-    onEdit(value); // Redirect to the edit page
-    setShowMenu(false);
+    onEdit(value);
+    setOpen(false);
   };
 
   return (
-    <div className="custom-cell">
-      <span onClick={() => setShowMenu(true)}>...</span>
-      {showMenu && (
-        <div className="popup-menu">
-          <div onClick={handleDelete}>Delete</div>
-          <div onClick={handleEdit}>Edit</div>
-        </div>
-      )}
+    <div>
+      <div
+        onClick={handleToggle}
+        ref={anchorRef}
+        style={{ cursor: 'pointer' }}
+      >
+        &#8942;
+      </div>
+      <Popper
+        open={open}
+        anchorEl={anchorRef.current}
+        placement="bottom-end"
+        disablePortal={true}
+        transition
+      >
+        {({ TransitionProps }) => (
+          <ClickAwayListener onClickAway={handleClose}>
+            <Paper>
+              <List>
+                <ListItem button onClick={handleDelete}>
+                  <ListItemIcon>
+                    <DeleteIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Delete" />
+                </ListItem>
+                <ListItem button onClick={handleEdit}>
+                  <ListItemIcon>
+                    <EditIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Edit" />
+                </ListItem>
+              </List>
+            </Paper>
+          </ClickAwayListener>
+        )}
+      </Popper>
     </div>
   );
 };
