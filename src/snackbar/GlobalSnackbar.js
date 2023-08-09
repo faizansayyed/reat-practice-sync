511fe5e5a4404aad90f8127a387c5d74236a955b
Faizan Sayyed <faizansayyed36@gmail.com>
2023-08-09 20:15:50 +0530

    getEditingCells

--- src/snackbar/GlobalSnackbar.js
+++ src/snackbar/GlobalSnackbar.js
@@ -11,6 +11,21 @@ const GlobalSnackbar = () => {
     dispatch(hideSnackbar());
   };
 
+  const handleCellClick = params => {
+    const isEditing = params.api
+      .getEditingCells()
+      .some(
+        cell =>
+          cell.rowIndex === params.node.rowIndex &&
+          cell.column === params.column
+      );
+
+    if (!isEditing) {
+      // Perform the click action for the column
+      console.log(`Cell clicked: ${params.value}`);
+    }
+  };
+
   return (
     <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
       <Alert onClose={handleClose} severity={type} sx={{ width: "100%" }}>
